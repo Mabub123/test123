cf21234a73d7b50b511adcac0a81618cabfa1ad0
Makhbuba <makhbuba18@gmail.com>
2023-05-16 19:48:48 -0400

    Initial commit

---
+++ src/lesson3/task1/ScannerTest.java
@@ -0,0 +1,22 @@
+package lesson3.task1;
+
+import java.util.Scanner;
+
+public class ScannerTest
+{
+    public static void main(String[] args) {
+
+
+        Scanner scanner=new Scanner()
+
+                String word = scanner.next();
+
+    }byte b = 50;
+
+        System.out.println("Byte: " + b);
+
+    short s = b;
+
+        System.out.println("Short: " + s);
+
+}
